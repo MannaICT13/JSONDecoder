cbeebeea3d590af1b0a1e9d548daefe29548d980
Manna <khmannaict13@gmail.com>
2019-07-03 22:05:20 +0600

    all commit

---
+++ JSONDecoder/TableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  TableViewCell.swift
+//  JSONDecoder
+//
+//  Created by USER on 3/22/19.
+//  Copyright © 2019 mCubes. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var lblName: UILabel!
+    
+    @IBOutlet weak var lblCapital: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+   
+}
+
+
+
+
+
+
+
+
+
+
+
--- JSONDecoder/ViewController.swift
+++ JSONDecoder/ViewController.swift
@@ -8,18 +8,101 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct JasonStruct : Decodable {
+    
+    let name : String
+    let capital : String
+    let region : String
+    let subregion : String
+}
 
+
+class ViewController : UIViewController,UITableViewDataSource,UITableViewDelegate {
+
+    var arrayData = [JasonStruct]()
+  
+   
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+      getDataFromCountryApi()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func getDataFromCountryApi(){
+        
+      
+        let url = URL(string: "https://restcountries.eu/rest/v2/all")
+        
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            
+            do{
+                if error == nil{
+                    
+                self.arrayData = try
+                    JSONDecoder().decode([JasonStruct].self, from: data!)
+                    
+                  
+                    for mainArray in self.arrayData{
+                        
+                        print(mainArray.name)
+                       
+                    }
+                    
+                }
+                
+            }catch{
+                
+                
+                print(error.localizedDescription)
+            }
+            
+            
+        }.resume()
+        
+        
+        
+        
+        
     }
-
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayData.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell :TableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        
+         cell.lblName.text = arrayData[indexPath.row].name
+        cell.lblCapital.text = arrayData[indexPath.row].capital
+        
+        return cell
+        
+        
+    }
+    
+    
+    
+  
 }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
